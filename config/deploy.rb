144c582447be763b873ae40391897a4ddb21279c
Kiminari Homma <u533u778@gmail.com>
2014-01-03 02:45:16 +0900

    debuged deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ namespace :deploy do
 
   task :setup_config do
     on roles(:app) do
-      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+      sudo "ln -nfs #{current_path}/config/nginx.conf /usr/local/nginx/sites-enabled/#{fetch(:application)}"
     end
   end
   after "deploy:started", "deploy:setup_config"
