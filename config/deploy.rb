272ba46b646815f3eaeb5e87d3030e78451c464d
Kiminari Homma <u533u778@gmail.com>
2014-01-03 02:51:14 +0900

    debuged deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,9 +30,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'unicorn:restart'
-    end
+    invoke 'unicorn:restart'
   end
 
   after :restart, :clear_cache do
