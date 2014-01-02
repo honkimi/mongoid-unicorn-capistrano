0e6ce5b5d878f7549c7dc117d69d7273004549f8
Kiminari Homma <u533u778@gmail.com>
2014-01-03 03:22:39 +0900

    debuged deploy.rb

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,9 +1,9 @@
 set :stage, :vagrant
 set :unicorn_env, "vagrant"
 
-role :app, %w{vagrant@127.0.0.1}
-role :web, %w{vagrant@127.0.0.1}
-role :db,  %w{vagrant@127.0.0.1}
+role :app, %w{unicorn@127.0.0.1}
+role :web, %w{unicorn@127.0.0.1}
+role :db,  %w{unicorn@127.0.0.1}
 
 set :ssh_options, :port => "2222"
 
